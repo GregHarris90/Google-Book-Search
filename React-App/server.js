d7e76d5ba660e928efd778ba6044e0b9325eda18
Greg Harris <ghart.productions@gmail.com>
2021-06-04 00:05:39 -0700

    built out server.js

--- React-App/server.js
+++ React-App/server.js
@@ -1,12 +1,32 @@
+// Dependencies
 const express = require("express");
+const mongoose = require("mongoose")
 const path = require("path");
+const routes = require("./routes")
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Define middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
+);
 
 // Send every request to the React app
 // Define any API routes before this runs
@@ -14,6 +34,7 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// Listening on PORT
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
